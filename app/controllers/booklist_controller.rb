a266c0d152790101fb3a56842dfb3fe117359503
kamocyc <ktgk.chr66g@gmail.com>
2020-06-03 09:32:49 +0900

    Modify code

--- app/controllers/booklist_controller.rb
+++ app/controllers/booklist_controller.rb
@@ -1,9 +1,13 @@
 class BooklistController < ApplicationController
   def hello
-    render :text => 'Hello!'
+    if(params.has_key?(:id) &&
+      params.has_key?(:random))
+      @id = params['id']
+      @random = params[:random]
+    end
   end
   
   def bye
-    render :text => 'bye!!'
+    @controller_message = "hoge"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get 'hello' => 'booklist#hello'
-  get 'bye' => 'booklist#bye'
+  match ':controller(/:action(/:id))', :via => :get
 end
