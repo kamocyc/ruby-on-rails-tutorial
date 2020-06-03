a9142aae289e63cee41c31580846f32022d55480
kamocyc <ktgk.chr66g@gmail.com>
2020-06-03 09:55:06 +0900

    Scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  match ':controller(/:action(/:id))', :via => :get
+  # match ':controller(/:action(/:id))', :via => :get
+  root 'users#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_120326) do
+ActiveRecord::Schema.define(version: 2020_06_03_004511) do
+
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", limit: 50, null: false
-    t.string "password", limit: 30, null: false
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
